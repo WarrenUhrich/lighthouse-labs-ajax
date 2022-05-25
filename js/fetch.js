9e37eb980e901771e6fdda0c5dc74c61545e6aec
Warren <hello@warren.codes>
2022-05-25 09:21:44 -0600

    Add example fetch request.

--- js/fetch.js
+++ js/fetch.js
@@ -1,1 +1,12 @@
-alert('My JavaScript is loaded!');
+const AJAX_URL = 'https://ghibliapi.herokuapp.com/films/';
+
+fetch(AJAX_URL)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
