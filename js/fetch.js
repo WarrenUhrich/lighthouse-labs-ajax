804fde4d8a1480ff66aaf10bb80d79dc7c9b39e0
Warren <hello@warren.codes>
2022-06-22 09:22:52 -0600

    Add to fetch example.

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,21 @@
-// alert('OUR SCRIPT IS LOADED!'); // See if our app is in-page!
+const AJAX_URL = 'https://ghibliapi.herokuapp.com/films/';
 
-fetch('https://ghibliapi.herokuapp.com/films/')
+fetch(AJAX_URL)
     .then((response) => {return response.json();})
-    .then((data) => {console.log(data);})
+    .then((data) => {
+        console.log(data); // Array of films.
+        
+        let outputHTML = '<ul>';
+
+        data.forEach((film) => {
+            outputHTML += '<li>';
+            outputHTML += `<h2>${film.title} / ${film.original_title} (${film.release_date})</h2>`;
+            outputHTML += `<p>${film.description}</p>`;
+            outputHTML += '</li>';
+        });
+
+        outputHTML += '</ul>';
+
+        document.body.innerHTML += outputHTML;
+    })
     .catch((error) => {console.error(error);});
